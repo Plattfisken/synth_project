1b55f0a4b416fb06c54eb8a153d3915843c56f56
Plattfisken <158032673+Plattfisken@users.noreply.github.com>
2024-02-26 21:04:02 -0300

    a

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ build:
 	clang -Wall -std=c99 ./src/*.c -o game -lSDL2 -g -O0
 
 run:
-	clang -Wall -std=c99 ./src/*.c -o game -lSDL2 -g -O0
+	clang -Wall -std=c99 ./src/*.c -o game -g -lSDL2 -O0
 	./game
 
 clean:
--- src/main.c
+++ src/main.c
@@ -25,27 +25,31 @@ struct {
   u32 pixels[SCREEN_WIDTH * SCREEN_HEIGHT];
   bool quit;
 } state;
-enum NoteName { 
-  none = 100, //Any value that can never be reached in another way
-  C = -9, 
-  CSharp = -8, 
-  D = -7, 
+enum NoteName {
+  none = 100, // Any value that can never be reached in another way
+  C = -9,
+  CSharp = -8,
+  D = -7,
   DSharp = -6,
-  E = -5, 
-  F = -4, 
-  FSharp = -3, 
-  G = -2, 
-  GSharp = -1, 
-  A = 0, 
-  ASharp = 1, 
+  E = -5,
+  F = -4,
+  FSharp = -3,
+  G = -2,
+  GSharp = -1,
+  A = 0,
+  ASharp = 1,
   B = 2
-}; 
-struct Note {
+};
+typedef struct Note {
   enum NoteName Name;
   i32 Octave;
-} noteToPlay = {none, 0};
+} Note; 
+Note noteToPlay = {none, 0};
+
 i32 selectedOctave = 0;
+u32 volume = 3000;
 int last_frame_time = 0;
+
 int initialize_window(void) {
 
   if (SDL_Init(SDL_INIT_EVERYTHING | SDL_INIT_AUDIO) != 0) {
@@ -53,9 +57,9 @@ int initialize_window(void) {
     return false;
   }
 
-  state.window =
-      SDL_CreateWindow("test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                       SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE);
+  state.window = SDL_CreateWindow(
+      "test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH,
+      SCREEN_HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE);
   if (!state.window) {
     fprintf(stderr, "Error creating SDL Window\n");
     return false;
@@ -77,7 +81,6 @@ int initialize_window(void) {
   }
   return true;
 }
-
 void process_input() {
   SDL_Event event;
   SDL_PollEvent(&event);
@@ -90,7 +93,7 @@ void process_input() {
       switch (event.key.keysym.sym) {
         case SDLK_ESCAPE:
           state.quit = true;
-          break; 
+          break;
         case SDLK_z:
           noteToPlay.Name = C;
           noteToPlay.Octave = selectedOctave;
@@ -149,6 +152,15 @@ void process_input() {
         case SDLK_q:
           --selectedOctave;
           break;
+        case SDLK_i:
+          if(volume >= 100)
+            volume -= 100;
+          else
+            volume = 0;
+          break;
+        case SDLK_o:
+          volume +=100;
+          break;
       }
       break;
     case SDL_KEYUP:
@@ -156,27 +168,7 @@ void process_input() {
       break;
   }
 }
-SDL_AudioDeviceID SDLInitAudio(i32 bufferSize, i32 samplesPerSecond) {
-  SDL_AudioSpec audioSettings = {0};
-  SDL_AudioSpec obtained;
-  audioSettings.freq = samplesPerSecond; 
-  audioSettings.format = AUDIO_S16LSB;
-  audioSettings.channels = 2;
-  audioSettings.samples = bufferSize;
-  audioSettings.callback = NULL; 
-
-  SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &audioSettings, &obtained, SDL_AUDIO_ALLOW_ANY_CHANGE);
-
-  if(obtained.format != AUDIO_S16LSB) {
-    fprintf(stderr, "Could not get S16LSB as sample format\n");
-    SDL_CloseAudio();
-    return 0;
-  }
-  return deviceId;
-}
-void setup() {
-}
-
+void setup() {}
 void update() {
   int time_to_wait = FRAME_TARGET_TIME - (SDL_GetTicks() - last_frame_time);
 
@@ -188,86 +180,123 @@ void update() {
   float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0f;
 
   last_frame_time = SDL_GetTicks();
-  
+
+  static f32 redOffset;
+  if (redOffset >= 1.0f)
+    redOffset = 0.0f;
+  redOffset += 0.001f;
+
+  static f32 blueOffset;
+  if (blueOffset >= 1.0f)
+    blueOffset = 0.0f;
+  blueOffset += 0.002f;
+
+  static f32 greenOffset;
+  if (greenOffset >= 1.0f)
+    greenOffset = 0.0f;
+  greenOffset += 0.003f;
+
   for (int i = 0; i < ARRAY_LENGTH(state.pixels); ++i) {
-    f32 procent = (f32)i / (f32)ARRAY_LENGTH(state.pixels);  
-    u32 red = (u32)(procent * 0xFF) << 24u;
-    u32 green = (u32)(procent * 0xFF) << 16u;
-    u32 blue = (u32)(procent * 0xFF) << 8u;
+    f32 procent = (f32)i / (f32)ARRAY_LENGTH(state.pixels);
+    u32 red = (u32)(procent * redOffset * 0xFF) << 24u;
+    u32 green = (u32)(procent * greenOffset * 0xFF) << 16u;
+    u32 blue = (u32)(procent * blueOffset * 0xFF) << 8u;
     u32 color = 0x000000FF | red | green | blue;
     state.pixels[i] = color;
   }
   SDL_UpdateTexture(state.texture, NULL, state.pixels, SCREEN_WIDTH * 4);
 }
-
 void render() {
   SDL_RenderClear(state.renderer);
   SDL_RenderCopy(state.renderer, state.texture, NULL, NULL);
   SDL_RenderPresent(state.renderer);
 }
-
 void destroy_window() {
   SDL_DestroyTexture(state.texture);
   SDL_DestroyRenderer(state.renderer);
   SDL_DestroyWindow(state.window);
 
-  SDL_CloseAudio(); //Not strictly necessary, SDL_Quit should already do this
+  SDL_CloseAudio(); // Not strictly necessary, SDL_Quit should already do this
   SDL_Quit();
 }
-f32 getHertz(struct Note note) {
+SDL_AudioDeviceID SDLInitAudio(i32 bufferSize, i32 samplesPerSecond) {
+  SDL_AudioSpec audioSettings = {0};
+  SDL_AudioSpec obtained;
+  audioSettings.freq = samplesPerSecond;
+  audioSettings.format = AUDIO_S16LSB;
+  audioSettings.channels = 2;
+  audioSettings.samples = bufferSize;
+  audioSettings.callback = NULL;
+
+  SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(
+      NULL, 0, &audioSettings, &obtained, SDL_AUDIO_ALLOW_ANY_CHANGE);
+
+  if (obtained.format != AUDIO_S16LSB) {
+    fprintf(stderr, "Could not get S16LSB as sample format\n");
+    SDL_CloseAudio();
+    return 0;
+  }
+  return deviceId;
+}
+f32 getHertz(Note note) {
   const f32 TwelvthSqrtOfTwoApprox = 1.059463f;
   i32 A = 440;
   i32 distanceFromA = note.Name;
-  f32 result = A * pow(TwelvthSqrtOfTwoApprox, distanceFromA) * pow(2, note.Octave);
+  f32 result =
+      A * pow(TwelvthSqrtOfTwoApprox, distanceFromA) * pow(2, note.Octave);
   return result;
 }
-u32 RoundFloat(f32 f)
-{
+u32 roundFloat(f32 f) {
   u32 result = (u32)(f + 0.5f);
   return result;
 }
-void playSineWave(SDL_AudioDeviceID deviceId, i32 samplesPerSecond, f32 hz) {
-}
-void playSquareWave(SDL_AudioDeviceID deviceId, i32 samplesPerSecond, f32 hz) {
-  i16 toneVolume = 3000;
+void playSoundWave(SDL_AudioDeviceID deviceId, i32 samplesPerSecond, f32 hz, i16 toneVolume) {
   static u32 runningSampleIndex = 0;
   f32 squareWavePeriod = samplesPerSecond / hz;
-  f32 halfSquareWavePeriod = squareWavePeriod / 2;
   i32 bytesPerSample = sizeof(i16) * 2;
   i32 targetQueueBytes = 4800 * bytesPerSample;
   i32 bytesToWrite = targetQueueBytes - SDL_GetQueuedAudioSize(deviceId);
-    if(bytesToWrite) {
-      void *soundBuffer = malloc(bytesToWrite);
-      i16 *sampleOut = (i16 *)soundBuffer;
-      i32 sampleCount = bytesToWrite/bytesPerSample;
-      for(i32 sampleIndex = 0; sampleIndex < sampleCount; ++sampleIndex) {
-        i16 sampleValue = ((runningSampleIndex++ / RoundFloat(halfSquareWavePeriod)) % 2) ? toneVolume : -toneVolume;
-        *sampleOut++ = sampleValue;
-        *sampleOut++ = sampleValue;
-      }
-      if(SDL_QueueAudio(deviceId, soundBuffer, bytesToWrite) != 0) {
-        fprintf(stderr,"%s", SDL_GetError());
-      }
-      free(soundBuffer);
+  if (bytesToWrite) {
+    void *soundBuffer = malloc(bytesToWrite);
+    i16 *sampleOut = (i16 *)soundBuffer;
+    i32 sampleCount = bytesToWrite / bytesPerSample;
+    for (i32 sampleIndex = 0; sampleIndex < sampleCount; ++sampleIndex) {
+      i16 sampleValue = sin(2 * M_PI * runningSampleIndex++ / squareWavePeriod) * toneVolume;  
+      *sampleOut++ = sampleValue;
+      *sampleOut++ = sampleValue;
     }
-    SDL_PauseAudioDevice(deviceId, 0);
+    //Square wave
+    // for (i32 sampleIndex = 0; sampleIndex < sampleCount; ++sampleIndex) {
+    //   i16 sampleValue =
+    //       ((runningSampleIndex++ / roundFloat(halfSquareWavePeriod)) % 2)
+    //           ? toneVolume
+    //           : -toneVolume;
+    //   *sampleOut++ = sampleValue;
+    //   *sampleOut++ = sampleValue;
+    // }
+    if (SDL_QueueAudio(deviceId, soundBuffer, bytesToWrite) != 0) {
+      fprintf(stderr, "%s", SDL_GetError());
+    }
+    free(soundBuffer);
+  }
+  SDL_PauseAudioDevice(deviceId, 0);
 }
 int main() {
   state.quit = !initialize_window();
   setup();
   i32 samplesPerSecond = 48000;
   SDL_AudioDeviceID deviceId = SDLInitAudio(4096, samplesPerSecond);
-  if(deviceId == 0) {
+  if (deviceId == 0) {
     fprintf(stderr, "Error when opening audio device\n");
   }
 
   while (!state.quit) {
-    
+
     process_input();
     update();
     render();
-    if(noteToPlay.Name != none)
-      playSquareWave(deviceId, samplesPerSecond, getHertz(noteToPlay));
+    if (noteToPlay.Name != none)
+      playSoundWave(deviceId, samplesPerSecond, getHertz(noteToPlay), volume);
     else
       SDL_ClearQueuedAudio(deviceId);
   }
